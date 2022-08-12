1750aa282b52eaf8cb25818cf8bd65f95111c3dc
Sebastian Gamarro <s.albertogamarro5@gmail.com>
2022-08-11 20:38:21 -0600

    saved changes

--- react-traker/src/App.js
+++ react-traker/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItemsList from "./components/Expenses/ExpenseItemsList";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2022, 3, 1),
+    },
+    {
+      id: "e2",
+      title: "New Tv",
+      amount: 799.49,
+      date: new Date(2022, 2, 1),
+    },
+    {
+      id: "e3",
+      title: "New Desk",
+      amount: 450,
+      date: new Date(2022, 5, 1),
+    },
+    {
+      id: "e4",
+      title: "Car Insuranse",
+      amount: 294.67,
+      date: new Date(2022, 7, 1),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItemsList items={expenses} />
     </div>
   );
 }
