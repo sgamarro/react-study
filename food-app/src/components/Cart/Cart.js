431bb555542b3f743f1318b616ee27628e9c657c
Sebastian Gamarro <s.albertogamarro5@gmail.com>
2022-08-31 18:04:18 -0600

    saved changes

--- food-app/src/components/Cart/Cart.js
+++ food-app/src/components/Cart/Cart.js
@@ -9,11 +9,27 @@ function Cart(props) {
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandeler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandeler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => {
         return (
-          <CartItem name={item.name} amount={item.amount} price={item.price} />
+          <CartItem
+            key={item.id}
+            name={item.name}
+            amount={item.amount}
+            price={item.price}
+            onRemove={cartItemRemoveHandeler.bind(null, item.id)}
+            onAdd={cartItemAddHandeler.bind(null, item)}
+          />
         );
       })}
     </ul>
@@ -21,7 +37,7 @@ function Cart(props) {
 
   return (
     <Modal onClose={props.onClose}>
-      {cartItems}
+      <div className={classes.list}>{cartItems}</div>
       <div className={classes.total}>
         <span>Total Amount:</span>
         <span>{totalAmount}</span>
--- food-app/src/components/Layout/HeaderCartButton.js
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -1,22 +1,43 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../context/context";
 
 function HeaderCartButton(props) {
   const cartCtx = useContext(CartContext);
-
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onShow} className={classes.button}>
+    <button onClick={props.onShow} className={buttonClasses}>
       <span className={classes["cart-icon"]}>
         <AiOutlineShoppingCart />
       </span>
       <span>Your Cart:</span>
-      <span>{numberOfCartItems}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
