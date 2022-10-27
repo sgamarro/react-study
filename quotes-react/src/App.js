6c8f9781aa1009897e8146a90c558f248ae3b69e
Sebastian Gamarro <s.albertogamarro5@gmail.com>
2022-10-26 22:48:45 -0600

    saved changes

--- quotes-react/src/App.js
+++ quotes-react/src/App.js
@@ -1,5 +1,28 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
-  return <div></div>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ quotes-react/src/pages/NewQuote.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewQuote = () => {
+  return (
+    <div>
+      <h1>NewQuote</h1>
+    </div>
+  );
+};
+
+export default NewQuote;
